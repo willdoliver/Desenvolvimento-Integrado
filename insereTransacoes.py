233d12c0034c1d155a20ac6a2dcd2098f1f6bab3
Patriky <patriky.galvao@gmail.com>
2018-10-19 02:44:43 -0300

    Script de insercao movimentacao- 720bi

--- insereTransacoes.py
+++ insereTransacoes.py
@@ -1,5 +1,6 @@
 import pymysql
 from random import randint
+import json
 
 id_cartao = []	
 data_transacao = []
@@ -9,9 +10,9 @@ def conexao():
 	global cur, conex 
 	try:
 		# CONEXAO SERVER RLE
-		conex = pymysql.connect(db= 'wolverine', user = 'wolverine', password = "@wolverine#", host = '200.134.10.221', port = 3306)
+		# conex = pymysql.connect(db= 'wolverine', user = 'wolverine', password = "@wolverine#", host = '200.134.10.221', port = 3306)
 		# CONEXAO LOCALHOST
-		#conex = pymysql.connect(db= 'wolv', user = 'root', password = "", host = '127.0.0.1', port = 3306)
+		conex = pymysql.connect(db= 'wolv', user = 'root', password = "", host = '127.0.0.1', port = 3306)
 
 		cur = conex.cursor() #Abre um cursor para executar operações no BD
 		print("****Conectou ao banco!****")
@@ -38,23 +39,32 @@ def insereMovimentacao():
 	for ano in range(1,6):
 		#Criando transações e datas randomicas - 12 meses
 		for mes in range(1,13):
+			for x in range(1,10000):				
 			#Criando transações e datas randomicas - 1200 transações
-			for transacao in range(0,1500):
-				data_transacao = "20"+str(ano+14)+"-"+str(mes)+"-"+str(randint(1,30))
-				valor_movimentacao = str(randint(-1000, 1000))+ "." + str(randint(0,99))
-				x = randint(0,40099)
-				idfk_cartao = id_cartao[x]
+				for transacao in range(0,1200):
+					data_transacao = "20"+str(ano+14)+"-"+str(mes)+"-"+str(randint(1,30))
+					valor_movimentacao = str(randint(-1000, 1000))+ "." + str(randint(0,99))
+					num = randint(0,40099)
+					idfk_cartao = id_cartao[num]
 
-				try:
-					sql_insercao = "INSERT INTO movimentacoes(data_mov, valor_mov, idfk_cartao) VALUES ('%s','%s','%s')" % (data_transacao,valor_movimentacao, idfk_cartao)
-					cur.execute(sql_insercao)
-					conex.commit()
-					cont += 1
-				except Exception as e:
-					fail += 1
-					continue
-				print(cont, " de 90000")
+					try:
+						sql_insercao = "INSERT INTO movimentacoes(data_mov, valor_mov, idfk_cartao) VALUES ('%s','%s','%s')" % (data_transacao,valor_movimentacao, idfk_cartao)
+						cur.execute(sql_insercao)
+						conex.commit()
+						cont += 1
+					except Exception as e:
+						fail += 1
+						continue
 	print(cont, " dados inseridos\n", fail, " nao inseridos")
+
+def insereCartao():
+	cartao1 = []
+	cartao2 = []	
+	cartao3 = []	
+	cartao4 = []
+
+
+
 
 
 if __name__ == '__main__':
